2c1550203413e0b82249fec2b66c445a5d41831a
Alvaro Torralba <alto@cs.aau.dk>
2024-09-17 17:54:25 +0200

    compress step

--- lab/compress_step.py
+++ lab/compress_step.py
@@ -1,6 +1,8 @@
 import shutil
 import tarfile
 from pathlib import Path
+import logging
+import os
 
 class CompressStep:
     def __init__(self, lab_experiment, target_folder, tmp_folder = None):
@@ -9,6 +11,8 @@ class CompressStep:
         self.tmp_folder = tmp_folder
 
     def __call__(self):
+        if not os.path.exists(self.lab_experiment.path):
+            logging.critical(f"Compress step could not find data at: {self.lab_experiment.path}.")
         if self.tmp_folder:
             Path(self.tmp_folder).mkdir(parents=True, exist_ok=True)
             output_filename = f"{self.tmp_folder}/{self.lab_experiment.name}.tar.gz"
@@ -16,7 +20,7 @@ class CompressStep:
             output_filename = f"{self.target_folder}/{self.lab_experiment.name}.tar.gz"
 
         with tarfile.open(output_filename, "w:gz") as tar:
-            tar.add(self.lab_experiment.path)
+            tar.add(self.lab_experiment.path, arcname=os.path.basename(self.lab_experiment.path))
 
         if self.tmp_folder:
-            shutil.move(output_filename, self.target_folder)
+            shutil.move(output_filename, f"{self.target_folder}/{self.lab_experiment.name}.tar.gz")
